71f62aca62fc84295e8c69fa77bf948e6eb81324
Ian W. Remmel <design@ianwremmel.com>
2015-04-30 16:30:00 -0700

    chore(build): karma/travis

--- browsers.js
+++ browsers.js
@@ -3,32 +3,52 @@
 /* eslint camelcase: [0] */
 
 module.exports = {
-  sl_chrome: {
+  sl_chrome_41_osx9: {
     base: 'SauceLabs',
-    browserName: 'chrome'
+    platform: 'OS X 10.9',
+    browserName: 'chrome',
+    version: '41'
   },
-  sl_firefox: {
+  sl_firefox_36_osx9: {
     base: 'SauceLabs',
-    browserName: 'firefox'
+    platform: 'OS X 10.9',
+    browserName: 'firefox',
+    version: '36'
   },
-  sl_safari_7: {
+  sl_safari_7_osx9: {
     base: 'SauceLabs',
+    platform: 'OS X 10.9',
     browserName: 'safari',
     version: '7'
   },
-  sl_safari_8: {
+  sl_safari_8_osx10: {
     base: 'SauceLabs',
+    platform: 'OS X 10.10',
     browserName: 'safari',
     version: '8'
   },
-  sl_ie_10: {
+  sl_ie_10_win7: {
     base: 'SauceLabs',
+    platform: 'Windows 7',
     browserName: 'internet explorer',
     version: '10'
   },
-  sl_ie_11: {
+  sl_ie_11_win7: {
     base: 'SauceLabs',
+    platform: 'Windows 7',
     browserName: 'internet explorer',
     version: '11'
+  },
+  sl_chrome_40_win7: {
+    base: 'SauceLabs',
+    platform: 'Windows 7',
+    browserName: 'chrome',
+    version: '41'
+  },
+  sl_firefox_36_win7: {
+    base: 'SauceLabs',
+    platform: 'Windows 7',
+    browserName: 'firefox',
+    version: '36'
   }
 };
